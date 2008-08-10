aed78b3bf9aed4ca7a86e4dac86e7a0bedcda074
Alexander Korsunsky <fat.lobyte9@gmail.com>
2008-08-10 16:25:23 +0000

    Switched to asynchronous state machine. WARNING: Application crashes "sometimes"

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -21,9 +21,305 @@
 #include <boost/bind.hpp>
 #include <boost/tokenizer.hpp>
 
+
+#include <boost/statechart/asynchronous_state_machine.hpp>
+#include <boost/statechart/simple_state.hpp>
+#include <boost/statechart/transition.hpp>
+#include <boost/statechart/event.hpp>
+#include <boost/statechart/custom_reaction.hpp>
+
 #include "protocol.hpp"
 
 
 using namespace nms;
 using namespace protocol;
+
+
+
+struct EventConnectRequest : boost::statechart::event<EventConnectRequest>
+{
+    std::wstring where;
+
+    EventConnectRequest(const std::wstring& _where)
+        : where(_where)
+    {}
+};
+
+struct EventConnectReport : boost::statechart::event<EventConnectRequest>
+{
+    bool success;
+
+    EventConnectReport(bool _success)
+        : success(_success)
+    {}
+};
+
+struct EventRcvdMsg : boost::statechart::event<EventRcvdMsg>
+{
+    std::wstring msg;
+
+    EventRcvdMsg(const std::wstring& _msg)
+        : msg(_msg)
+    { }
+};
+
+
+struct EventSendMsg : boost::statechart::event<EventSendMsg>
+{
+    std::wstring msg;
+
+    EventSendMsg(const std::wstring& _msg)
+        : msg(_msg)
+    { }
+};
+
+struct EventDisconnect : boost::statechart::event<EventDisconnect>
+{};
+
+
+
+
+struct StateUnconnected;
+struct StateConnected;
+
+
+struct ProtocolMachine
+    : public boost::statechart::asynchronous_state_machine<
+        ProtocolMachine,
+        StateUnconnected
+        >
+{
+    boost::function1 <void, const control::ProtocolNotification&>
+        notification_callback;
+
+    ProtocolMachine(my_context ctx,
+                    const boost::function1
+                        <void, const control::ProtocolNotification&>&
+                        _notification_callback) throw()
+        : my_base(ctx),
+            notification_callback(_notification_callback)
+    {}
+};
+
+
+
+
+
+struct StateIdle;
+struct StateTryingConnect;
+
+struct StateUnconnected
+    : public boost::statechart::simple_state<StateUnconnected,
+                                                ProtocolMachine,
+                                                StateIdle>
+{ };
+
+
+struct StateIdle
+    : public boost::statechart::simple_state<StateIdle,
+                                                StateUnconnected>
+{
+    typedef boost::statechart::custom_reaction< EventConnectRequest > reactions;
+
+    boost::statechart::result react(const EventConnectRequest& request)
+    {
+        std::cout<<"Trying to connect to "<<std::string(request.where.begin(), request.where.end())<<"\n";
+        return transit<StateTryingConnect>();
+    }
+};
+
+struct StateTryingConnect
+    : public boost::statechart::simple_state<StateTryingConnect,
+                                                StateUnconnected>
+{
+    typedef boost::mpl::list<
+        boost::statechart::custom_reaction< EventConnectReport >,
+        boost::statechart::custom_reaction<EventDisconnect>
+    > reactions;
+
+    boost::statechart::result react(const EventConnectReport& rprt)
+    {
+        if (rprt.success)
+        {
+            context<ProtocolMachine>()
+                .notification_callback(
+                    control::ReportNotification
+                        <control::ProtocolNotification::ID_CONNECT_REPORT>()
+                    );
+
+            return transit<StateConnected>();
+        }
+        else
+        {
+            context<ProtocolMachine>()
+                .notification_callback(
+                    control::ReportNotification
+                        <control::ProtocolNotification::ID_CONNECT_REPORT>
+                        (L"Sorry, connection failed.\n")
+                    );
+
+            return transit<StateUnconnected>();
+        }
+    }
+
+    boost::statechart::result react(const EventDisconnect&)
+    {
+        context<ProtocolMachine>()
+            .notification_callback(
+                control::ProtocolNotification
+                    (control::ProtocolNotification::ID_DISCONNECTED)
+                );
+
+        return transit<StateUnconnected>();
+    }
+
+};
+
+
+struct StateConnected
+    : public boost::statechart::simple_state<StateConnected,
+                                                ProtocolMachine>
+{
+    typedef boost::mpl::list<
+        boost::statechart::custom_reaction< EventSendMsg >,
+        boost::statechart::custom_reaction< EventRcvdMsg >,
+        boost::statechart::custom_reaction<EventDisconnect>
+    > reactions;
+
+    StateConnected()
+    {
+        std::cout<<"We are connected!\n";
+    }
+
+
+    boost::statechart::result react(const EventSendMsg& msg)
+    {
+        std::cout<<"Sending message: ";
+        std::cout<<std::string(msg.msg.begin(), msg.msg.end())<<'\n';
+
+        return discard_event();
+    }
+
+    boost::statechart::result react(const EventRcvdMsg& msg)
+    {
+        context<ProtocolMachine>()
+            .notification_callback(
+                control::ReceivedMsgNotification
+                    (msg.msg)
+                );
+
+        return discard_event();
+    }
+
+    boost::statechart::result react(const EventDisconnect&)
+    {
+        context<ProtocolMachine>()
+            .notification_callback(
+                control::ProtocolNotification
+                    (control::ProtocolNotification::ID_DISCONNECTED)
+                );
+
+        return transit<StateUnconnected>();
+    }
+
+};
+
+
+
+
+
+NMSProtocol::NMSProtocol(const boost::function1<void, const control::ProtocolNotification&>&
+                            _notification_callback)
+            throw()
+
+    : notification_callback(_notification_callback),
+        machine_scheduler(true)
+{
+    // create an event processor for our state machine
+    event_processor =
+        machine_scheduler.create_processor<ProtocolMachine>(
+            _notification_callback
+            );
+
+    // initiate the event processor
+    machine_scheduler.initiate_processor(event_processor);
+
+    // machine_thread is in state "not-a-thread", so we will use the move
+    // semantics provided by boost::thread to create a new thread and assign it
+    // the variable
+#if 1
+    machine_thread = boost::thread(
+        boost::bind(
+            &boost::statechart::fifo_scheduler<>::operator(),
+            &machine_scheduler,
+            0
+            )
+        );
+#endif
+
+}
+
+NMSProtocol::~NMSProtocol()
+{
+    // stop the network machine
+    machine_scheduler.terminate();
+
+    try {
+
+        // give the thread a few seconds time to join
+        machine_thread.timed_join(boost::posix_time::millisec(threadwait_ms));
+
+    }
+    catch(...)
+    {}
+
+    // if the thread finished, return. otherwise try to kill the thread
+    if (machine_thread.get_id() == boost::thread::id())
+        return;
+
+    machine_thread.interrupt();
+}
+
+
+void NMSProtocol::connect_to(const std::wstring& id)
+    throw(std::runtime_error, ProtocolError)
+{
+#if 1
+    boost::intrusive_ptr<EventConnectRequest>
+    connect_request(new EventConnectRequest(id));
+
+    boost::intrusive_ptr<EventConnectReport>
+    connect_reply(new EventConnectReport(true));
+
+
+    machine_scheduler.queue_event(event_processor, connect_request);
+    machine_scheduler.queue_event(event_processor, connect_reply);
+#endif
+}
+
+
+
+void NMSProtocol::send(const std::wstring& msg)
+    throw(std::runtime_error, ProtocolError)
+{
+
+    boost::intrusive_ptr<EventSendMsg>
+    send_evt(new EventSendMsg(msg));
+
+    machine_scheduler.queue_event(event_processor, send_evt);
+}
+
+
+void NMSProtocol::disconnect()
+    throw(std::runtime_error, ProtocolError)
+{
+#if 1
+    boost::intrusive_ptr<EventDisconnect>
+    disconnect_evt(new EventDisconnect);
+
+    machine_scheduler.queue_event(event_processor, disconnect_evt);
+#endif
+}
+
+
 
--- src/protocol.hpp
+++ src/protocol.hpp
@@ -35,16 +35,13 @@
 #include <boost/asio.hpp>
 
 #include <boost/thread/thread.hpp>
-#include <boost/thread/recursive_mutex.hpp>
+#include <boost/thread/mutex.hpp>
 
-
-#include <boost/statechart/state_machine.hpp>
-#include <boost/statechart/simple_state.hpp>
-#include <boost/statechart/transition.hpp>
-#include <boost/statechart/event.hpp>
-#include <boost/statechart/custom_reaction.hpp>
+#include <boost/statechart/asynchronous_state_machine.hpp>
 
 #include <boost/function.hpp>
+
+
 
 #include "control.hpp"
 
@@ -68,7 +65,7 @@ public:
         : std::runtime_error("Unknown Communication Protocol Error")
     { }
 
-    /** Constructor. 
+    /** Constructor.
     * @param str The error message
     */
     ProtocolError(const char* str) throw()
@@ -90,245 +87,74 @@ public:
 
 
 
-struct EventConnectRequest : boost::statechart::event<EventConnectRequest>
-{
-    std::wstring where;
-};
-
-struct EventConnectReport : boost::statechart::event<EventConnectRequest>
-{
-    bool success;
-    std::wstring reason;
-};
-
-struct EventRcvdMsg : boost::statechart::event<EventRcvdMsg>
-{
-    std::wstring msg;
-};
-
-
-struct EventSendMsg : boost::statechart::event<EventSendMsg>
-{
-    std::wstring msg;
-};
-
-struct EventDisconnect : boost::statechart::event<EventDisconnect>
-{};
-
-
-
-
-struct StateUnconnected;
-struct StateConnected;
-
-
-struct ProtocolMachine
-    : public boost::statechart::state_machine<ProtocolMachine, StateUnconnected>
-{
-    boost::function1 <void, const control::ProtocolNotification&>
-        notification_callback;
-
-    ProtocolMachine(const boost::function1
-                        <void, const control::ProtocolNotification&>&
-                        _notification_callback) throw()
-        : notification_callback(_notification_callback)
-    {}
-};
-
-
-
-
-
-struct StateIdle;
-struct StateTryingConnect;
-
-struct StateUnconnected
-    : public boost::statechart::simple_state<StateUnconnected,
-                                                ProtocolMachine,
-                                                StateIdle>
-{ };
-
-
-struct StateIdle
-    : public boost::statechart::simple_state<StateIdle,
-                                                StateUnconnected>
-{
-    typedef boost::statechart::custom_reaction< EventConnectRequest > reactions;
-
-    boost::statechart::result react(const EventConnectRequest& request)
-    {
-        std::cout<<"Trying to connect to "<<std::string(request.where.begin(), request.where.end())<<"\n";
-        return transit<StateTryingConnect>();
-    }
-};
-
-struct StateTryingConnect
-    : public boost::statechart::simple_state<StateTryingConnect,
-                                                StateUnconnected>
-{
-    typedef boost::mpl::list<
-        boost::statechart::custom_reaction< EventConnectReport >,
-        boost::statechart::custom_reaction<EventDisconnect>
-    > reactions;
-
-    boost::statechart::result react(const EventConnectReport& rprt)
-    {
-        if (rprt.success)
-        {
-            context<ProtocolMachine>()
-                .notification_callback(
-                    control::ReportNotification
-                        <control::ProtocolNotification::ID_CONNECT_REPORT>()
-                    );
-
-            return transit<StateConnected>();
-        }
-        else
-        {
-            context<ProtocolMachine>()
-                .notification_callback(
-                    control::ReportNotification
-                        <control::ProtocolNotification::ID_CONNECT_REPORT>
-                        (L"Sorry, connection failed.\n")
-                    );
-
-            return transit<StateUnconnected>();
-        }
-    }
-
-    boost::statechart::result react(const EventDisconnect&)
-    {
-        context<ProtocolMachine>()
-            .notification_callback(
-                control::ProtocolNotification
-                    (control::ProtocolNotification::ID_DISCONNECTED)
-                );
-
-        return transit<StateUnconnected>();
-    }
-
-};
-
-
-struct StateConnected
-    : public boost::statechart::simple_state<StateConnected,
-                                                ProtocolMachine>
-{
-    typedef boost::mpl::list<
-        boost::statechart::custom_reaction< EventSendMsg >,
-        boost::statechart::custom_reaction< EventRcvdMsg >,
-        boost::statechart::custom_reaction<EventDisconnect>
-    > reactions;
-
-    StateConnected()
-    {
-        std::cout<<"We are connected!\n";
-    }
-
-    
-    boost::statechart::result react(const EventSendMsg& msg)
-    {
-        std::cout<<"Sending message: "<<std::string(msg.msg.begin(), msg.msg.end())<<'\n';
-        return discard_event();
-    }
-
-    boost::statechart::result react(const EventRcvdMsg& msg)
-    {
-        context<ProtocolMachine>()
-            .notification_callback(
-                control::ReceivedMsgNotification
-                    (msg.msg)
-                );
-
-        return discard_event();
-    }
-
-    boost::statechart::result react(const EventDisconnect&)
-    {
-        context<ProtocolMachine>()
-            .notification_callback(
-                control::ProtocolNotification
-                    (control::ProtocolNotification::ID_DISCONNECTED)
-                );
-
-        return transit<StateUnconnected>();
-    }
-
-};
-
-
-
 /** Client Communication Protocol.
 * @ingroup proto
-* Use this class to create a black box that handles all the network stuff for 
+* Use this class to create a black box that handles all the network stuff for
 * you.
 * Request are handled by the public functions connect_to, send and disconnect.
 * Replies will be dispatched to the callback function you supply in the constructor.
 */
 class NMSProtocol
 {
-    boost::mutex machine_mutex; /**< the mutex for the state machine*/
-    ProtocolMachine protocol_machine; /**< The state machine itself*/
-    
+    /** An own thread for the State Machine*/
+    boost::thread machine_thread;
+
+    /** The scheduler for the asynchronous state machine */
+    boost::statechart::fifo_scheduler<> machine_scheduler;
+
+    /** The event processor handle for the state machine */
+    boost::statechart::fifo_scheduler<>::processor_handle event_processor;
+
     /** The function object that will be called, if an event occurs.*/
     boost::function1<void, const control::ProtocolNotification&>
         notification_callback;
 
+    /** How long to wait for the thread to join */
+    enum { threadwait_ms = 3000 };
 public:
 
     /** Constructor.
-    * Initializes the Network machine.
-    * @param _notification_callback The callback function where the events will 
+    * Creates a thread and initializes the Network machine.
+    * @param _notification_callback The callback function where the events will
     * be dispatched
     */
     NMSProtocol(const boost::function1<void,
                                         const control::ProtocolNotification&>&
-                    _notification_callback) throw()
-        : notification_callback(_notification_callback),
-            protocol_machine(_notification_callback)
-    {
-        protocol_machine.initiate();
-    }
+                    _notification_callback) throw();
+
+    /** Destructor.
+    * Stops the Network machine and destroys the thread.
+    */
+    ~NMSProtocol();
 
     /** Connect to a remote site.
      * @param id The string representation of the address of the remote site
-    * @throws std::runtime_error if a ressource could not be allocated. 
+    * @throws std::runtime_error if a ressource could not be allocated.
     * e.g. a threading resource.
     * @throws ProtocolError if a networking error occured
      */
     void connect_to(const std::wstring& id)
-        throw(std::runtime_error, ProtocolError)
-    {
-        boost::lock_guard<boost::mutex> guard(machine_mutex);
+        throw(std::runtime_error, ProtocolError);
 
-        protocol_machine.process_event(EventConnectRequest(id));
-        protocol_machine.process_event(EventConnectReport(true));
-    }
+
 
     /** Send message to connected remote site.
      * @param msg The message you want to send
-    * @throws std::runtime_error if a ressource could not be allocated. 
+    * @throws std::runtime_error if a ressource could not be allocated.
     * e.g. a threading resource.
     * @throws ProtocolError if a networking error occured
      */
     void send(const std::wstring& msg)
-        throw(std::runtime_error, ProtocolError)
-    {
-        boost::lock_guard<boost::mutex> guard(machine_mutex);
-        protocol_machine.process_event(EventSendMsg(msg));
-    }
+        throw(std::runtime_error, ProtocolError);
+
 
     /** Disconnect from the remote site.
-    * @throws std::runtime_error if a ressource could not be allocated. 
+    * @throws std::runtime_error if a ressource could not be allocated.
     * e.g. a threading resource.
     * @throws ProtocolError if a networking error occured
     */
     void disconnect()
-        throw(std::runtime_error, ProtocolError)
-    {
-        boost::lock_guard<boost::mutex> guard(machine_mutex);
-        protocol_machine.process_event(EventDisconnect());
-    }
+        throw(std::runtime_error, ProtocolError);
 
 };
 
