fc07bee38e6b21690ca9d08a5f65fe627c410339
Alexander Korsunsky <fat.lobyte9@gmail.com>
2012-02-27 19:27:47 +0100

    test/servnode: Implement testing over socket
    Currently, only the receiving function is checked.

--- test/servnode/test_connected-client.cpp
+++ test/servnode/test_connected-client.cpp
@@ -2,6 +2,24 @@
 
 /*
  *   nuke-ms - Nuclear Messaging System
+ *   Copyright (C) 2012  Alexander Korsunsky
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// test_connected-client.cpp
+
+/*
+ *   nuke-ms - Nuclear Messaging System
  *   Copyright (C) 2011  Alexander Korsunsky
  *
  *   This program is free software: you can redistribute it and/or modify
@@ -19,8 +37,10 @@
 
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/thread.hpp>
 
 #include "neartypes.hpp"
+#include "servnode/connected-client.hpp"
 
 #include "testutils.hpp"
 
@@ -30,72 +50,123 @@ using boost::asio::ip::tcp;
 
 DECLARE_TEST("class ConnectedClient")
 
+// Test against the identity of this data
+static std::string data_in_received;
 
-void acceptHandler(const boost::system::error_code& accept_error)
+void receiveCallback(
+    std::shared_ptr<servnode::ConnectedClient> client,
+    std::shared_ptr<SerializedData> data
+)
 {
-    TEST_ASSERT(!accept_error);
-    std::cout<<(!accept_error ?
-        "Someone connected!\n" :
-        "Something went wrong with accepting\n")
-    ;
+    data_in_received.assign(NearUserMessage(*data)._stringwrap._message_string);
+
+    std::cout<<"server: Data received: \""<<data_in_received<<
+        "\". Shutting down client.\n";
+    client->shutdown();
 }
 
-void connectHandler(const boost::system::error_code& connect_error)
-{
-    TEST_ASSERT(!connect_error);
-    std::cout<<
-        (!connect_error ? "Connection succeeded.\n" : "Connection failed.\n");
-}
+void disconnectCallback(
+    std::shared_ptr<servnode::ConnectedClient> client
+)
+{ std::cout<<"server: Client "<<client->connection_id<<" disconnected.\n"; }
 
-struct SendHandler
-{
-    std::shared_ptr<byte_traits::byte_sequence> seq;
 
-    void operator() (
-        const boost::system::error_code& send_error,
-        std::size_t bytes_transferred
-    )
+
+struct MockServer {
+    boost::asio::io_service io_service;
+
+    MockServer()
+    : acceptor_v4(io_service, tcp::endpoint{tcp::v4(), 34443})
+    { }
+
+    void run()
     {
-        TEST_ASSERT(!send_error && bytes_transferred == seq->size());
-        std::cout<<(!send_error ? "Message sent.\n" : "send failed\n");
+        tcp::socket socket(io_service);
+
+        // start accept, bind socket to the handler
+        boost::system::error_code accept_error;
+        acceptor_v4.accept(socket, accept_error);
+
+        if (!accept_error)
+        {
+            std::cout<<"server: Someone connected!\n";
+            this->client_container = servnode::ConnectedClient::makeInstance(
+                0,
+                std::move(socket),
+                this->io_service,
+                receiveCallback,
+                disconnectCallback
+            );
+        }
+        else
+            std::cout<<"server: Accept failed with: "<<accept_error.message()<<'\n';
+
+        TEST_ASSERT(!accept_error);
+
+        io_service.run();
     }
+
+// private:
+    std::shared_ptr<servnode::ConnectedClient> client_container;
+    tcp::acceptor acceptor_v4;
 };
 
-void async_sendMessage(tcp::socket& sock)
+
+void sendMessage(tcp::socket& sock, const std::string& data)
 {
-    NearUserMessage msg{std::move(std::string("Wazzup!!"))};
+    SegmentationLayer<NearUserMessage> msg{NearUserMessage(data)};
     auto seq = std::make_shared<byte_traits::byte_sequence>(msg.size());
     msg.fillSerialized(seq->begin());
 
-    std::cout<<"Sending Message... ";
-    boost::asio::async_write(sock, boost::asio::buffer(*seq), SendHandler{seq});
+    std::cout<<"client: Sending Message...\n";
+
+    boost::system::error_code send_error;
+    std::size_t bytes_transferred =
+        boost::asio::write(sock, boost::asio::buffer(*seq), send_error);
+
+    std::cout<<(!send_error ? "client: Message sent.\n" : "send failed\n");
+    TEST_ASSERT(!send_error && bytes_transferred == seq->size());
 }
+
 
 
 int main()
 {
-    boost::asio::io_service io_service;
+    // -------- SERVER CODE ---------
+    std::cout<<"Initializing server\n";
 
-    tcp::acceptor acceptor_v4{io_service, tcp::endpoint{tcp::v4(), 34443}};
-    tcp::socket acc_socket{io_service};
+    MockServer server;
 
-    tcp::socket con_socket{io_service};
+    // Start server with ConnectedClient code
+    boost::thread server_thread(boost::bind(&MockServer::run, &server));
 
-    // start accept, bind socket to the handler
-    acceptor_v4.async_accept(
-        acc_socket,
-        &acceptHandler
-    );
+
+    // -------- CLIENT CODE ---------
+    std::cout<<"Initializing client\n";
+
+    boost::asio::io_service client_io_service;
+    tcp::socket con_socket{client_io_service};
 
     // connect to the local socket
-    con_socket.async_connect(
+    boost::system::error_code connect_error;
+    con_socket.connect(
         tcp::endpoint{boost::asio::ip::address::from_string("127.0.0.1"),34443},
-        &connectHandler
+        connect_error
     );
 
-    async_sendMessage(con_socket);
+    TEST_ASSERT(!connect_error);
+    if (!connect_error)
+        std::cout<<"client: Connection succeeded.\n";
+    else
+        return 1;
+
+    con_socket.set_option(boost::asio::socket_base::linger(true, 2000));
+    sendMessage(con_socket, "Wazzzuuppp???");
 
 
-    io_service.run();
+    server_thread.join();
+
+    TEST_ASSERT(data_in_received == "Wazzzuuppp???");
+
     return CONCLUDE_TEST();
 }
